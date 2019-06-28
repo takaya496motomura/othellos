7ebc0c786f8c3bb93d3b6ac6904234924da10dcc
takaya496motomura <50699517+takaya496motomura@users.noreply.github.com>
2019-06-28 18:44:49 +0900

    文字列が入力されたときのエラーの追加
    choose.cpp>57行目
    文字列が入力されたときに、入力を繰り返す処理を追加しました。
    小数点や負の値での例外処理は加えていませんが、月曜日に追加致します。

--- othello_3/othello_3/files/choose.cpp
+++ othello_3/othello_3/files/choose.cpp
@@ -44,15 +44,22 @@ int choosepiece() {
 int choosemode() {
 	int x;
 
-	do {
+//	do {
 
-		printf("Player1 would choose battle mode.(CPU battle:0,Interpersonal battle:1)\n\n");
+//		printf("Player1 would choose battle mode.(CPU battle:0,Interpersonal battle:1)\n\n");
 		// ユーザ入力を受け取る
-		printf("please input: ");
-		scanf("%d", &x);
+//		printf("please input: ");
+//		scanf("%d", &x);
 
+//	} while (x > 1);
 
-	} while (x > 1);
+	while (1) {
+		printf("Player1 would choose battle mode.(CPU battle:0,Interpersonal battle:1)\n\n");
+		if (scanf("%d", &x) == 1 && x >= 0 && x <= 1) break; //修正中
+		if (feof(stdin)) return 1;
+		printf("数値が正しくありません。もう一度入力してください。\n");
+		scanf("%*[^\n]");
+	}
 
 	if (x == 0) {
 
